724a654ae9c28e172c5d669d78fc7e495ef9951b
RajGGoswami <109051791+RajGGoswami@users.noreply.github.com>
2026-01-12 21:07:26 +1300

    Create data.py

---
+++ data.py
@@ -0,0 +1,89 @@
+# This file stores quiz question data.
+# Keeping data separate from logic makes the program cleaner and easier to update.
+# Questions are structured as dictionaries so they can be easily converted into objects later.
+
+question_data = [
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "The Windows ME operating system was released in the year 2000.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "Linus Torvalds created Linux and Git.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "Time on Computers is measured via the EPOX System.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "RAM stands for Random Access Memory.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "The Python programming language gets its name from the British comedy group \"Monty Python.\"",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "The programming language \"Python\" is based off a modified version of \"JavaScript\".",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "In most programming languages, the operator ++ is equivalent to the statement \"+= 1\".",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "Ada Lovelace is often considered the first computer programmer.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }
+]
+
+# More questions can be fetched from:
+# https://opentdb.com/api_config.php
